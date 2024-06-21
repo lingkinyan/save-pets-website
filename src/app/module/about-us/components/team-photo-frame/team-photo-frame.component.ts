ae035fad9d72c508b50c34065fa3bf7638b9b198
lingkinyan <kyling21@gmail.com>
2024-06-21 19:01:42 -0400

    image size updated

--- src/app/module/about-us/components/team-photo-frame/team-photo-frame.component.ts
+++ src/app/module/about-us/components/team-photo-frame/team-photo-frame.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IntersectionComponents } from '../../../../common-components/class/intersection.component';
 
 @Component({
   selector: 'app-team-photo-frame',
@@ -6,7 +7,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './team-photo-frame.component.html',
   styleUrl: './team-photo-frame.component.less',
 })
-export class TeamPhotoFrameComponent {
+export class TeamPhotoFrameComponent extends IntersectionComponents{
   @Input() imgSrc = '';
   @Input() name = '';
   @Input() position = '';
